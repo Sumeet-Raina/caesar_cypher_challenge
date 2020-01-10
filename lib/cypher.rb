5cf479c27320bd9c38bba73d1cf532ba870103fd
sumeet978 <searchsumeet@gmail.com>
2020-01-10 03:20:16 +0000

    Refactored the code by removing magic number

--- lib/cypher.rb
+++ lib/cypher.rb
@@ -1,9 +1,12 @@
 class Cypher
+
+  ENCRYPTION_VALUE =  3
+
   def string_to_cypher(string)
     if string =~ /\d/ 
       raise "Your string contains a number"
     else
-      cypher_string = string.split("").map{ |i| (cypher = i.ord + 3).chr } 
+      cypher_string = string.split("").map{ |i| (cypher = i.ord + ENCRYPTION_VALUE).chr } 
       cypher_string.join
     end
   end
