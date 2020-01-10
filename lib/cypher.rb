66ae0e00b918edc94f0cfcfd78c4209d61fb2094
sumeet978 <searchsumeet@gmail.com>
2020-01-10 03:14:27 +0000

    It raises an error when a number is passed to string

--- lib/cypher.rb
+++ lib/cypher.rb
@@ -1,6 +1,10 @@
 class Cypher
   def string_to_cypher(string)
-    cypher_string = string.split("").map{ |i| (cypher = i.ord + 3).chr } 
-    cypher_string.join
+    if string =~ /\d/ 
+      raise "Your string contains a number"
+    else
+      cypher_string = string.split("").map{ |i| (cypher = i.ord + 3).chr } 
+      cypher_string.join
+    end
   end
 end
