f7438ceb490464e39a3a7646eb2ce175f0f1e556
sumeet978 <searchsumeet@gmail.com>
2020-01-10 02:24:48 +0000

    Added second test

--- spec/cypher_spec.rb
+++ spec/cypher_spec.rb
@@ -6,4 +6,9 @@ describe Cypher do
     cypher = Cypher.new
     expect(cypher.string_to_cypher("a")).to eq("d")
   end
+  it "Cyphers a given alphabet" do
+    cypher = Cypher.new
+    expect(cypher.string_to_cypher("f")).to eq("i")
+  end
+
 end 
