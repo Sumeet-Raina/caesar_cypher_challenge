de5ebaf93de356af06e1824b350eff01535ed649
sumeet978 <searchsumeet@gmail.com>
2020-01-10 02:39:11 +0000

    Refactored the code

--- lib/cypher.rb
+++ lib/cypher.rb
@@ -1,13 +1,5 @@
 class Cypher
   def string_to_cypher(string)
-    if string == 'a'
-      return 'd'
-    end
-    if string == 'b'
-      return 'e'
-    end
-    if string == 'h'
-      return 'k'
-    end
+    (string.ord + 3).chr
   end
 end
